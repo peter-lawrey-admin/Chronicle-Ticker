a37601179a77aaf4ec4bd07a6e8bc910df8fc9ae
rogersimmons <roger.simmons@higherfrequencytrading.com>
2018-10-05 09:28:31 +0100

    Add test for Ticker.nanoClock() timings

---
+++ src/test/java/net/openhft/chronicle/ticker/NanoClockTest.java
@@ -0,0 +1,24 @@
+package net.openhft.chronicle.ticker;
+
+import org.junit.Test;
+
+public class NanoClockTest {
+
+    @Test
+    public void perfTest()
+    {
+        long start = System.nanoTime();
+        long end = start + 2_000_000_000;
+        int batch = 1000;
+        long tests = 0;
+        long blackHole = 0;
+        do {
+            for (int i = 0; i < batch; i++)
+                blackHole = Ticker.nanoClock();
+            tests += batch;
+        } while (System.nanoTime() < end);
+        long time = System.nanoTime() - start;
+        double avgTime = (double) time / tests;
+        System.out.printf("NanoClockTest: Average time to call nanoClock() %.1f ns%n", avgTime);
+    }
+}
